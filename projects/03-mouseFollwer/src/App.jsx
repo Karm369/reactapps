e5b3bdea768733966c13b3702e280699786044b6
Johel Benavides <johelbc369@gmail.com>
2023-10-30 23:01:21 -0600

    02 pack deply on gato

--- projects/03-mouseFollwer/src/App.jsx
+++ projects/03-mouseFollwer/src/App.jsx
@@ -1,55 +1,51 @@
 import { useState,useEffect } from 'react'
 
-
-
 function App() {
-  const [enabled, setEnabled] = useState(false)
-  const [position,setPosition] = useState({x:0, y:0})
-  //const [display, setDisplay]= useState({"none":})
-  //const adisplay = () =>{enabled? "visible": "hidden:"}
-
-  useEffect(()=>
-  {
   
-    console.log("efecto ",{enabled})
+    const [enabled, setEnabled] = useState(false)
+    const [position,setPosition] = useState({x:0, y:0})
 
-    const handleMove =(event)=>
-    {
-      const {clientX,clientY}=event
-      console.log("handleMove ",{clientX,clientY})
-      setPosition({x:clientX, y:clientY})
-    }
+    useEffect(()=>{
+  
+      console.log("efecto ",{enabled})
 
-    if(enabled){
-      window.addEventListener("pointermove",handleMove)
-    
-    }
+      //event to set x,y from pointermove function 
+      const handleMove =(event)=>{
+        const {clientX,clientY}=event
+        //console.log("handleMove ",{clientX,clientY})
+        setPosition({x:clientX, y:clientY})
+      }
+      //suscription
+      if(enabled){
+        window.addEventListener("pointermove",handleMove)
       
-
-    return()=>{window.removeEventListener("pointermove",handleMove)}
+      }
+      //clean
+      return()=>{window.removeEventListener("pointermove",handleMove)}
  
-  },[enabled])
+    },[enabled])
 
-  return (
-    <main>
-    <div style={{ 
-    //visibility:`${adisplay}`,
-    position: "absolute",
-    backgroundColor: "#09f",
-    borderRadius:"50%",
-    opacity:0.3,
-    pointerEvents:"none",
-    left:-20,
-    top:-20,
-    width:40,
-    height: 40,
-    transform:`translate(${position.x}px, ${position.y}px)`
-    }
-       
-    }/>
-    <button onClick={()=>setEnabled(!enabled)}> {enabled?"Desactivar ":"Activar "}Zona</button>
-    </main>
-  )
+    return (
+      <main>
+      <div style={{ 
+      visibility:`${enabled? "visible":"hidden"}`,
+      position: "absolute",
+      //TODO: add change color relative to x position
+      backgroundColor: "#09f",
+      borderRadius:"50%",
+      opacity:0.3,
+      pointerEvents:"none",
+      left:-20,
+      top:-20,
+      width:40,
+      height: 40,
+      transform:`translate(${position.x}px, ${position.y}px)`
+      }
+        
+      }/>
+      <button onClick={()=>setEnabled(!enabled)}> {enabled?"Desactivar ":"Activar "}Zona</button>
+      </main>
+    )
 }
 
 export default App
